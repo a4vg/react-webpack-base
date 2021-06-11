229543bb4c1d2387adf574bd4c7ca0226e798d06
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2021-06-10 23:17:27 -0500

    Add react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import logo from "../assets/logo.svg";
 import "./App.scss";
@@ -6,22 +7,49 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.jsx</code> and save to reload.
+          </p>
+
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link className="App-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="App-link" to="/about">
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home page</h2>;
+}
+
+function About() {
+  return <h2>About page</h2>;
 }
 
 export default App;
